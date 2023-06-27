8e66452185d43d8a58da6cf03d30c383ad794f6d
Eliúde Vemba <sweeteliude@gmail.com>
2023-06-27 23:54:35 +0100

    Update Employee
    CRUD 03/04

--- client/employee/lib/pages/form_page.dart
+++ client/employee/lib/pages/form_page.dart
@@ -17,6 +17,7 @@ class _FormPageState extends State<FormPage> {
   final _formKey = GlobalKey<FormState>();
   final _formData = <String, Object>{};
   bool _isLoading = false;
+  bool _isUpgradeMode = false;
 
   void _submit() async {
     final isValid = _formKey.currentState?.validate() ?? false;
@@ -28,19 +29,21 @@ class _FormPageState extends State<FormPage> {
     setState(() => _isLoading = true);
 
     final employee = Employee(
-      id: Random().nextInt(1000) + 1,
+      id: _isUpgradeMode ? _formData['id'] as int : Random().nextInt(1000) + 1,
       name: _formData['name'] as String,
       phone: _formData['phone'].toString(),
       department: _formData['department'] as String,
       address: _formData['address'] as String,
     );
 
-    var response = await context.read<EmployeeProvider>().addEmployee(employee);
+    var response = _isUpgradeMode
+        ? await context.read<EmployeeProvider>().updateEmployee(employee)
+        : await context.read<EmployeeProvider>().addEmployee(employee);
 
     if (context.mounted) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
-          content: Text(response ? 'Successfully added' : 'New employee fail'),
+          content: Text(response ? 'Successfully submited' : 'Fail to submit'),
           duration: const Duration(seconds: 3),
           action: SnackBarAction(
             label: 'Dismiss',
@@ -56,9 +59,18 @@ class _FormPageState extends State<FormPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
       appBar: AppBar(
-        title: const Text('New employee'),
+        title: Text(_isUpgradeMode ? 'Upgrade employee' : 'New employee'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() => _isUpgradeMode = !_isUpgradeMode);
+            },
+            icon: Icon(_isUpgradeMode ? Icons.add : Icons.upgrade),
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(28.0),
@@ -66,6 +78,55 @@ class _FormPageState extends State<FormPage> {
           key: _formKey,
           child: Column(
             children: [
+              if (_isUpgradeMode)
+                TextFormField(
+                  onTapOutside: (event) {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                  },
+                  validator: (value) {
+                    final phone = int.tryParse(
+                          value?.replaceFirst(RegExp(','), '') ?? '-1',
+                        ) ??
+                        -1;
+
+                    if (phone <= 0) {
+                      return 'Invalid id';
+                    }
+
+                    if (phone.toString().startsWith('0')) {
+                      return 'Number must not start with 0';
+                    }
+
+                    return null;
+                  },
+                  onSaved: (id) => _formData['id'] = int.parse(
+                    id?.replaceFirst(RegExp(','), '') ?? '0',
+                  ),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [
+                    LengthLimitingTextInputFormatter(9),
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  decoration: InputDecoration(
+                    label: const Text('Id'),
+                    fillColor: const Color.fromARGB(50, 255, 255, 255),
+                    border: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(Radius.circular(16)),
+                      borderSide: BorderSide(
+                          width: 1,
+                          color: Theme.of(context).colorScheme.primary),
+                    ),
+                    enabledBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(16)),
+                      borderSide: BorderSide(
+                        width: 2,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 20),
               TextFormField(
                 onTapOutside: (event) {
                   FocusManager.instance.primaryFocus?.unfocus();
@@ -151,7 +212,7 @@ class _FormPageState extends State<FormPage> {
                       -1;
 
                   if (phone <= 0) {
-                    return 'Invalid price';
+                    return 'Invalid phone number';
                   }
 
                   if (phone.toString().length != 9) {
@@ -233,14 +294,14 @@ class _FormPageState extends State<FormPage> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ElevatedButton(
-                    onPressed: _isLoading ? null : _submit,
-                    child: const Text('Submeter'),
-                  ),
-                  ElevatedButton(
                     onPressed: () {
                       _formData.clear();
                     },
-                    child: const Text('Cancelar'),
+                    child: const Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: _isLoading ? null : _submit,
+                    child: const Text('Submit'),
                   ),
                 ],
               ),
--- client/employee/lib/provider/employee_provider.dart
+++ client/employee/lib/provider/employee_provider.dart
@@ -97,9 +97,44 @@ class EmployeeProvider extends ChangeNotifier {
     }
   }
 
-  Future<void> updateEmployee(Employee employee) async {
-    // TODO: Implement update employee
-    return Future.delayed(const Duration(seconds: 3));
+  Future<bool> updateEmployee(Employee employee) async {
+    String body = '''
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:com="http://com.springbootsoap.allapis">
+   <soapenv:Header/>
+   <soapenv:Body>
+      <com:updateEmployeeRequest>
+         <com:employeeInfo>
+            <com:employeeId>${employee.id}</com:employeeId>
+            <com:name>${employee.name}</com:name>
+            <com:department>${employee.department}</com:department>
+            <com:phone>${employee.phone}</com:phone>
+            <com:address>${employee.address}</com:address>
+         </com:employeeInfo>
+      </com:updateEmployeeRequest>
+   </soapenv:Body>
+</soapenv:Envelope>
+''';
+
+    try {
+      var response = await http.post(
+        Uri.parse(SOAPBASEURL),
+        headers: {
+          'Content-Type': 'text/xml;charset=utf-8',
+          'SOAPACTION': '',
+        },
+        body: body,
+      );
+
+      if (response.statusCode < 300 && response.statusCode >= 200) {
+        setEmployee(null);
+        return true;
+      }
+
+      return false;
+    } catch (e) {
+      log(e.toString());
+      return false;
+    }
   }
 
   Future<void> deleteEmployee(Employee employee) async {
